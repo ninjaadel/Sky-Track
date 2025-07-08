1fbb7dc61b570286a2b7152b95b80d70d75c8029
ninjaadel <iurchenkoadel@gmail.com>
2025-07-08 09:53:16 +0300

    DAY 12 i did skeleton for flightlist its work when page loading whithb timeout

--- src/components/flightCard/flightList.jsx
+++ src/components/flightCard/flightList.jsx
@@ -1,22 +1,35 @@
 import FlightCard from "./flightCard";
 import { flights } from "../../data/flight";
 import { Filter } from "../filters/filter";
-import { useMemo, useState } from "react";
-
+import { useEffect, useMemo, useState } from "react";
+import { CardSkeleton } from "../ui/cardskeleton";
 export default function FlightList() {
   const [fromCountry, setFromCountry] = useState();
-  const FiltredCountry = useMemo(() => {
+  const [showSkeleton, setShowSkeleton] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowSkeleton(false);
+    }, 3000);
+
+    return () => clearTimeout(timer); // cleanup!
+  }, []);
+
+  const filteredCountry = useMemo(() => {
     if (!fromCountry) return flights;
     return flights.filter((flight) => flight.from.country === fromCountry);
   }, [fromCountry]);
+
   return (
     <div className="w-sm relative -mt-23">
-      <Filter fromCountry={fromCountry} setFromCountry={setFromCountry} />
       <div className="space-y-4">
-        {FiltredCountry.map((flight, i) => (
-          <FlightCard key={i} flight={flight} />
-        ))}
+        {showSkeleton
+          ? filteredCountry.map((_, i) => <CardSkeleton key={i} />)
+          : filteredCountry.map((flight, i) => (
+              <FlightCard key={i} flight={flight} />
+            ))}
       </div>
+      <Filter fromCountry={fromCountry} setFromCountry={setFromCountry} />
     </div>
   );
 }
